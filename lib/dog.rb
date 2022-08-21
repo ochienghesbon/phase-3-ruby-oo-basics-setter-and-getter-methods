2bed190e43bc28a14f4339c4aae0165f1d452358
ochienghesbon <ochienghesbon@student.moringaschool.com>
2022-08-21 08:09:44 +0300

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,16 @@
 class Dog
-
+    # Define a name getter and a name= setter method for the instance variable @name.
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name
+    end
+    def breed=(breed)
+        @breed=breed
+    end
+    def breed
+        @breed
+    end
 end
+# Define a breed getter and a breed= setter method for the instance variable @breed
--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,17 @@
 class Person
+    # Define a name getter and a name= setter method for the instance variable @name.
+    def name=(name)
+        @name=name
+    end
+    def name
+        @name
+    end
 
+    # Define a job getter and a job= setter method for the instance variable @job.
+    def job=(job)
+        @job=job
+    end
+    def job
+        @job
+    end
 end
